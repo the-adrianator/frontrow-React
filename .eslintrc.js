2a24092272b47bc21963a60a044d102a0ae6ee46
CodeGenie.dev <adrian@codegenie.dev>
2022-09-22 20:29:32 +0100

    file and folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,11 @@ module.exports = {
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      modules: true
+    }
   },
   plugins: [
     'react',
