33eeccc1c859cb970350521a7d30e5059943c284
CodeGenie.dev <adrian@codegenie.dev>
2022-09-23 07:02:39 +0100

    navbar and sidebar structure

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+import { SideBar } from '..';
+
+import useStyles from './styles';
 
 const NavBar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px');
+  const theme = useTheme();
+  const isAuthenticated = true;
+
   return (
-    <div>NavBar</div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              color="inherit"
+              edge="start"
+              style={{outline: 'none'}}
+              onClick={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button 
+                color="inherit" 
+                component={Link}
+                to={`/profile/:id`}
+                className={classes.linkButton}
+                onClick={() => {}}
+                >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar 
+                  style={{width: 30, height: 30}}
+                  alt="Profile"
+                  src='https://avatarairlines.com/wp-content/uploads/2020/05/Male-placeholder.jpeg'
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={mobileOpen}
+              onClose={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{keepMounted: true}}
+            >
+              <SideBar setMobileOpen={setMobileOpen} />  
+            </Drawer>
+          ) : (
+            <Drawer classes={{paper: classes.drawerPaper}} variant="permanent" open>
+              <SideBar setMobileOpen={setMobileOpen} />  
+            </Drawer>
+          )}
+        </nav>
+      </div>
+    </>
   )
 }
 
