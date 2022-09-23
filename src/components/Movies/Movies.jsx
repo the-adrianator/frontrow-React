48b18ff8d33a2980619fd785abea0223c1dc15ef
CodeGenie.dev <adrian@codegenie.dev>
2022-09-23 19:35:25 +0100

    Rendering movies and movie view

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,13 +2,37 @@ import React, {useState, useEffect} from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import {useGetMoviesQuery} from '../../services/TMDB'
+import {useGetMoviesQuery} from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  const {data} = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if(isFetching) {
+    return (
+      <Box display='flex' justifyContent='center'>
+        <CircularProgress size='4rem'/>
+      </Box>
+    )
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display='flex' alignItems='center' mt='20px'>
+        <Typography variant='h4'> No movies found matching that name.
+        <br />
+        Please refine your search.
+        </Typography>
+      </Box>
+    )
+  }
+
+  if (error) return 'An error has occured.';
+
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data}/>
+    </div>
   )
 }
 
