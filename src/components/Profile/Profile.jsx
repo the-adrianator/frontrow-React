bd157911c464af4efe7062780e3468a609ec477d
CodeGenie.dev <adrian@codegenie.dev>
2022-09-24 18:07:15 +0100

    Finalized authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { userSelector } from '../../features/auth';
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 
 const Profile = () => {
+  const {user} = useSelector(userSelector);
+
+  const favoriteMovies = []
+
+  const logOut = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  }
+
+  console.log(user.username);
   return (
-    <div>Profile</div>
+    <Box>
+      <Box display='flex' justifyContent='space-between'>
+        <Typography variant='h4' gutterBottom>My Profile</Typography>
+        <Button color='inherit' onClick={logOut}>
+          Log Out &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length 
+        ? <Typography variant='h5'>Add movies to favorites to see them here!</Typography>
+        : (
+          <Box>
+            FAVORITE MOVIES
+          </Box>
+        )
+      }
+    </Box>
   )
 }
 
