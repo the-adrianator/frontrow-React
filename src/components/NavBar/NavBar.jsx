0ee7f730c77b7f0f95ce9a42946865b1e75a37a3
CodeGenie.dev <adrian@codegenie.dev>
2022-09-24 13:19:03 +0100

    User authentication

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,41 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 import { SideBar, Search } from '..';
-
+import { fetchToken, moviesApi, createSessionId } from '../../utils';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser, userSelector } from '../../features/auth';
 import useStyles from './styles';
 
 const NavBar = () => {
+  const {isAuthenticated, user} = useSelector(userSelector);
   const [mobileOpen, setMobileOpen] = useState(false);
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px');
   const theme = useTheme();
-  const isAuthenticated = true;
+  const dispatch = useDispatch();
+
+  const token = localStorage.getItem('request_token');
+  const sessionIdFromLocalStorage = localStorage.getItem('session_id')
+
+  useEffect(() => {
+    const logInUser = async () => {
+      if(token) {
+        if(sessionIdFromLocalStorage) {
+          const {data: userData} = await moviesApi.get(`/account?session_id=${sessionIdFromLocalStorage}`)
+          dispatch(setUser(userData));
+        } else {
+          const sessionId = await createSessionId();
+          const {data: userData} = await moviesApi.get(`/account?session_id=${sessionId}`)
+          dispatch(setUser(userData));
+        }
+      }
+    }
+
+    logInUser();
+  }, [token])
 
   return (
     <>
@@ -35,14 +58,14 @@ const NavBar = () => {
           {!isMobile && <Search />}
           <div>
             {!isAuthenticated ? (
-              <Button color="inherit" onClick={() => {}}>
+              <Button color="inherit" onClick={fetchToken}>
                 Login &nbsp; <AccountCircle />
               </Button>
             ) : (
               <Button 
                 color="inherit" 
                 component={Link}
-                to={`/profile/:id`}
+                to={`/profile/${user.id}`}
                 className={classes.linkButton}
                 onClick={() => {}}
                 >
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,6 +3,8 @@ import {Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box,
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 import { useDispatch, useSelector } from 'react-redux';
+import redLogo from '../../assets/images/redLogo.png';
+import blueLogo from '../../assets/images/blueLogo.png';
 
 import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 import { useGetGenresQuery } from '../../services/TMDB';
@@ -15,9 +17,6 @@ const categories = [
   { label: 'Upcoming', value: 'upcoming' }
 ]
 
-const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
-const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
-
 const SideBar = ({setMobileOpen}) => {
   const {genreIdOrCategoryName} = useSelector((state) => state.currentGenreOrCategory);
   const theme = useTheme();
@@ -29,8 +28,8 @@ const SideBar = ({setMobileOpen}) => {
     <>
       <Link to="/" className={classes.imageLink}>
         <img 
-          src={theme.palette.mode === 'light' ? redLogo : blueLogo}
-          alt="Filmpire Logo" 
+          src={theme.palette.mode === 'light' ? blueLogo : redLogo}
+          alt="Frontrow Logo" 
           className={classes.image}
         />
       </Link>
