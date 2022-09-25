e0feb60adf83dd661728bfb6efcfb5d5c378f860
CodeGenie.dev <adrian@codegenie.dev>
2022-09-25 11:32:44 +0100

    Movie information page

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -14,7 +14,7 @@ const Movies = () => {
 
   if(isFetching) {
     return (
-      <Box display='flex' justifyContent='center'>
+      <Box display='flex' justifyContent='center' alignItems='center'>
         <CircularProgress size='4rem'/>
       </Box>
     )
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -32,11 +32,16 @@ export const tmdbApi = createApi({
         // * Get Popular Movies (default)
         return `movie/popular?page=${page}&api_key=${tmdbApiKey}`
       }
+    }),
+    // * Get Movie
+    getMovie: builder.query({
+      query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`
     })
   })
 });
 
 export const {
   useGetGenresQuery,
-  useGetMoviesQuery
+  useGetMoviesQuery,
+  useGetMovieQuery
 } = tmdbApi;
